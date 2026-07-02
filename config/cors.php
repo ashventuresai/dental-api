50851aeccae97d27322549bed7c8adffdec642ad
asyrafa <asyrafa@sunway.edu.my>
2026-07-02 17:47:09 +0800

    [Add] Railway init app

--- config/cors.php
+++ config/cors.php
@@ -20,7 +20,8 @@ return [
     'allowed_methods' => ['*'],
 
     // 'allowed_origins' => ['*'],
-    'allowed_origins' => ['http://localhost:3000'],
+    'allowed_origins' => ['https://dental-web-test.vercel.app'],
+    // 'allowed_origins' => env('ALLOWED_ORIGINS'),
 
     'allowed_origins_patterns' => [],
 
---
+++ railway/init-app.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+php artisan migrate --force
+
+php artisan optimize:clear
+
+php artisan config:cache
+
+php artisan route:cache
+
+php artisan view:cache
