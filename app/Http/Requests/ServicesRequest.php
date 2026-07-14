3f39367dad529642443e64d123ae3452bfec13cb
asyrafa <asyrafa@sunway.edu.my>
2026-07-14 09:54:06 +0800

    [Fix] Max amount for service description

--- app/Http/Requests/ServicesRequest.php
+++ app/Http/Requests/ServicesRequest.php
@@ -26,7 +26,7 @@ class ServicesRequest extends FormRequest
             'service_name'              => ['required','string','max:100'],
             'service_category'          => ['nullable','string','max:50'],
             'service_price'             => ['required','integer','min:0'],
-            'service_description'       => ['nullable','string','max:255'],
+            'service_description'       => ['nullable','string','max:500'],
             'service_active'            => ['required','string','max:10'],
         ];
 
